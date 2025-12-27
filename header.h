f2f1ad7f3513f60f7417d952c8d2386fb1d50251
cya <zaskiaamelianur@gmail.com>
2025-12-27 18:21:40 +0700

    finalup header.h

--- header.h
+++ header.h
@@ -1,32 +1,39 @@
-#ifndef HEADER_H
-#define HEADER_H
+#ifndef HEADER_H_INCLUDED
+#define HEADER_H_INCLUDED
 
 #include <iostream>
 #include <string>
 using namespace std;
 
-typedef string infotype;
-typedef struct Node *adrNode;
-
-
 struct Node {
     int priority;
-    infotype info;
-    adrNode left;
-    adrNode right;
+    string info;
+    Node *left;
+    Node *right;
 };
 
+typedef Node* adrNode;
 
-adrNode createNode(int priority, infotype info);
-adrNode findMin(adrNode root);
-adrNode insertNode(adrNode root, int priority, infotype info);
-adrNode deleteNode(adrNode root, int priority);
+adrNode createNode(int priority, string info);
+adrNode insertNode(adrNode root, int priority, string info);
 adrNode searchNode(adrNode root, int priority);
+adrNode findMin(adrNode root);
+adrNode deleteNode(adrNode root, int priority);
 adrNode findSuccessor(adrNode root, int priority);
 adrNode findPredecessor(adrNode root, int priority);
+
 void inorder(adrNode root);
+void preorder(adrNode root);
+void postorder(adrNode root);
+
 int countAtLevel(adrNode root, int level);
 void searchRange(adrNode root, int low, int high);
-void printTree(adrNode root, int space, int gap = 10);
+void printTree(adrNode root, int space = 0, int gap = 10);
+
+int height(adrNode root);
+bool isBalanced(adrNode root);
+
+void clearTree(adrNode &root);
 
 #endif
+
