60e6a896341bed409173843344edbf9f6b88ad67
Macaa-aw <nimacha22@gmail.com>
2025-12-27 15:14:44 +0700

    Update header.h

--- header.h
+++ header.h
@@ -1,30 +1,32 @@
-#ifndef HEADER_H_INCLUDED
-#define HEADER_H_INCLUDED
+#ifndef HEADER_H
+#define HEADER_H
 
 #include <iostream>
 #include <string>
 using namespace std;
 
+typedef string infotype;
+typedef struct Node *adrNode;
+
+
 struct Node {
     int priority;
-    string info;
-    Node *left;
-    Node *right;
+    infotype info;
+    adrNode left;
+    adrNode right;
 };
 
-typedef Node* adrNode;
 
-/* ==== PROTOTYPE ==== */
-adrNode createNode(int priority, string info);
-adrNode insertNode(adrNode root, int priority, string info);
-adrNode searchNode(adrNode root, int priority);
+adrNode createNode(int priority, infotype info);
 adrNode findMin(adrNode root);
+adrNode insertNode(adrNode root, int priority, infotype info);
 adrNode deleteNode(adrNode root, int priority);
+adrNode searchNode(adrNode root, int priority);
 adrNode findSuccessor(adrNode root, int priority);
 adrNode findPredecessor(adrNode root, int priority);
 void inorder(adrNode root);
 int countAtLevel(adrNode root, int level);
 void searchRange(adrNode root, int low, int high);
-void printTree(adrNode root, int space, int gap);
+void printTree(adrNode root, int space, int gap = 10);
 
 #endif
